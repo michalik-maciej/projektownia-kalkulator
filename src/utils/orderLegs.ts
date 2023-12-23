c0f278dae8a6ed79739442f12add534fa58fdcb9
Maciej Michalik <halniak24@gmail.com>
2023-12-23 01:10:29 +0100

    share legs in gondola

--- src/utils/orderLegs.ts
+++ src/utils/orderLegs.ts
@@ -11,15 +11,17 @@ import { aggregateOrder } from "./aggregateOrder"
 export const orderLegs = (data: FormCollectionType[]) => {
   const aggregateByLegs = ({
     height,
+    variant,
     stands,
-  }: FormSubCollectionType & FormStandType) => {
+  }: FormCollectionType & FormSubCollectionType & FormStandType) => {
     const leg = filter(({ h }) => h === height, legs)[0]
     const number = sumBy("numberOfStands", stands) + 1
 
     return [
       {
         description: `${leg?.h} / ${leg?.w} / ${leg?.d}`,
-        number,
+        // share legs in gondola and impulse collections between sides
+        number: variant === "P" ? number : Math.ceil(0.5 * number),
         price: number * leg.price,
       },
     ]
